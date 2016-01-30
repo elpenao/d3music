145ecfb06fdbc0dc1f6373010cc2159cea3839ea
elpenao <elpenao@gmail.com>
2016-01-30 17:42:45 -0500

    load top 10 tracks for an artist

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 var rp = require('request-promise');
 var fs = require('fs');
 var jsonfile = require('jsonfile');
+var morgan = require('morgan');
 
 var client_id = '9e7aae0e58094ed4bba106c1ca37ee6c'; // Your client id
 var client_secret = '724bbab1fc9748799ed69c50dc20dcee';
@@ -17,16 +18,17 @@ var bodyParser = require('body-parser');
 var app = express(); 
 var server = app.listen(3001);
 
+app.use(morgan('combined'))
 app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 swig.setDefaults({ cache: false });
 
-app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/public',express.static('public'));
 
 app.get('/', function (req, res, next) {
 	// // body...
@@ -36,16 +38,30 @@ app.get('/', function (req, res, next) {
 app.post('/search', function(req, res, next){
 	var firstName = req.body.artist.split(" ")[0]
 	var lastName = req.body.artist.split(" ")[1]
+	var artist;
 	rp('https://api.spotify.com/v1/search?q=' + firstName + '%20' + lastName + '&type=artist')
 		.then(function(searchResult){
 			var result = JSON.parse(searchResult);
-			var artist = result.artists.items[0];
+			// res.json(result)
+			artist = result.artists.items[0];
 			var data = { "Artist": artist.name, "Popularity": artist.popularity };
-			jsonfile.writeFile('artist.json', data, function (err) {
-			  console.error(err)
+			jsonfile.writeFileSync('./public/artist.txt', data)
+			// res.render('artist', {
+			// 	artist: artist
+			// })
+		})
+		.then(function(){
+			//  https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE"
+			return rp('https://api.spotify.com/v1/artists/' + artist.id + '/top-tracks?country=SE')
+		})
+		.then(function(tracks){
+			tracks = JSON.parse(tracks);
+			// res.json(tracks)
+			// data = [{ "Artist": artist.name, "Popularity": artist.popularity }];
+			jsonfile.writeFile('./public/tracks.txt', tracks.tracks)
+			res.render('artist', {
+				artist: artist
 			})
-			// fs.writeFileSync('artist.json', data);
-			res.render('artist')
 		})
 		.catch()
 })
